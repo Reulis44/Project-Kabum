81daca936a8650167f958b7a1115151766877f3e
Alan Guilherme Bispo da Silva <alangui.1910@hotmail.com>
2022-12-21 01:18:48 -0300

    Function checks for computers with a value less 4k

--- Kabum/dataframe.py
+++ Kabum/dataframe.py
@@ -7,6 +7,7 @@ class DataFrame:
     def __init__(self) -> None:
         self.create_dataframe()
         self.remove_repeated_values()
+        self.selections_and_frequency()
     
     
     def create_dataframe(self):
@@ -17,5 +18,14 @@ class DataFrame:
     def remove_repeated_values(self):
         self.dt.drop_duplicates(inplace= True) # Implace serve para salvar as alterações que fizemos no Data Frame.
         
+    
+    def selections_and_frequency(self):
+        # Verificando se há computadores com o valor menor ou igual a 4000
+        selection = (self.dt['Valor'] <= 4000)
+        # Verificando quantos computadores tem o valor menor ou igual a 4000
+        self.value = selection.shape[0]
         
+
+    
+
 DataFrame()
